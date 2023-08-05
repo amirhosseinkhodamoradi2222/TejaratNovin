6e0be55c7abd50c5448941c423668ae797bd7171
Amir Hossein Khodamoradi <128831822+amirhosseinkhodamoradi2222@users.noreply.github.com>
2023-08-05 08:40:22 -0700

    'Start1.0.1'

---
+++ my-app/components/Navbar/Navbar.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+
+export default function Navbar() {
+  return (
+    <>
+      <main className="shadow-md py-3">
+        <div className="container mx-auto">
+          <div className="flex justify-between ">
+            <div>sss</div>
+            <div>sss</div>
+            <div>sss</div>
+          </div>
+        </div>
+      </main>
+    </>
+  );
+}
--- my-app/pages/index.js
+++ my-app/pages/index.js
@@ -1,19 +1,17 @@
-import Form from "../components/Navbar/form";
-
 
 export default function Home(props) {
   return (
     <>
-      <h1 className="text-center bg-gray-500">Slam</h1>
-      <Form />
       <p>{props.amir}</p>
     </>
   );
 }
-export async function getServerSideProps () {
-  return{
-    props : {
-      amir : 'Amir Hossein'
-    }
-  }
+
+//server
+export async function getServerSideProps() {
+  return {
+    props: {
+      amir: "Amir Hossein",
+    },
+  };
 }
