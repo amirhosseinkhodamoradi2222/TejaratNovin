38e656d1e8e142778c2018cfb9d618f7bcad1cc7
amirhossein <developer.amir.khodamoradi@gmail.com>
2023-08-16 15:32:56 +0330

    V1.1.36

--- my-app/pages/_app.js
+++ my-app/pages/_app.js
@@ -1,14 +1,24 @@
 import "./globals.css";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/page";
+import { NextSeo } from "next-seo";
+import icon from "../assets/image/logo.jpeg";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="fonts bg-backgroundColor">
-      <Navbar />
-      <Component {...pageProps} />
-      <Footer />
-    </div>
+    <>
+      <NextSeo themeColor="#1D2241" />
+      <Head>
+
+      <link rel="shortcut icon" href={icon} />
+      </Head>
+      <div className="fonts bg-backgroundColor">
+        <Navbar />
+        <Component {...pageProps} />
+        <Footer />
+      </div>
+    </>
   );
 }
 
