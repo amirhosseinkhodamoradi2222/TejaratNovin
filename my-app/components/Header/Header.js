2f39b87d9af8a2627da9e6514a10969c37132fe9
amirhossein <developer.amir.khodamoradi@gmail.com>
2023-08-17 20:49:22 +0330

    V1.1.41

--- my-app/components/Header/Header.js
+++ my-app/components/Header/Header.js
@@ -3,14 +3,14 @@ import logo3 from "../../assets/image/logo3.png";
 import Linkedin from "../Linkedin/Linkedin";
 import "swiper/css/navigation";
 import { Navigation } from "swiper/modules";
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState } from "react";
 // Import Swiper React components
-import { Swiper, SwiperSlide } from 'swiper/react';
-import Typed from 'typed.js';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { TypeAnimation } from "react-type-animation";
 
 // Import Swiper styles
-import 'swiper/css/effect-cards';
-import { EffectCards } from 'swiper/modules';
+import "swiper/css/effect-cards";
+import { EffectCards } from "swiper/modules";
 
 function Header() {
   return (
@@ -20,42 +20,58 @@ function Header() {
           <div className="grid grid-rows-1 grid-cols-1 lg:grid-cols-2 md:grid-cols-2 gap-4">
             <div className="mr-4 lg:mr-0">
               <h1 className="font-Medium text-whate text-md lg:text-3xl">
-                شرکت تجارت نوین تولید و عرضه مستقیم کاشی و سرامیک
+                <TypeAnimation
+                  sequence={[
+                    "شرکت تجارت نوین تولید و عرضه مستقیم کاشی و سرامیک",
+                    5000,
+                    "شرکت تجارت نوین",
+                  ]}
+                  wrapper="span"
+                  cursor={false}
+                  speed={50}
+                  className="font-Medium text-whate text-md lg:text-3xl"
+                  repeat={Infinity}
+                />
               </h1>
               <h2 className="font-Regular mt-8 text-whate lg:text-2xl text-md">
                 لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ
               </h2>
-              <p className="font-light my-6 text-justify text-whate">
-              لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم استلورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است
+              <p className="font-light my-6 mb-8 text-justify text-whate">
+                لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با
+                استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله
+                در ستون و سطرآنچنان که لازم استلورم ایپسوم متن ساختگی با تولید
+                سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است.
+                چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم
+                است
               </p>
               <Linkedin />
             </div>
             <div className="mr-4">
-            <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-      >
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-        <SwiperSlide>
-        <Image src={logo3} width={500} height={500} />
-        </SwiperSlide>
-      </Swiper>
+              <Swiper
+                effect={"cards"}
+                grabCursor={true}
+                modules={[EffectCards]}
+                className="mySwiper"
+              >
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+                <SwiperSlide>
+                  <Image src={logo3} width={500} height={500} />
+                </SwiperSlide>
+              </Swiper>
             </div>
           </div>
         </div>
