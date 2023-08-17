7c3e2a663d888924fd727e281e20de3f5f971df3
amirhossein <developer.amir.khodamoradi@gmail.com>
2023-08-17 15:29:25 +0330

    V1.1.39

--- my-app/components/About/About.js
+++ my-app/components/About/About.js
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import React from "react";
 import logo3 from "../../assets/image/logo3.png";
+import { AiOutlineArrowLeft } from "react-icons/ai";
+import Link from "next/link";
 
 export default function About() {
   return (
@@ -10,8 +12,11 @@ export default function About() {
           <Image src={logo3} />
         </div>
         <div>
-          <p>zxcxzczxc</p>
-          <p>zxcxzczxc</p>
+          <p className="font-bold text-4xl">معرفی تیم شرکت <span className="text-primaryColor">تجارت نوین</span></p>
+          <p className="font-light text-justify my-8">
+          سه سال پیش دی ام روم، یک تیم یک نفره و تنها سرمایه اون تجربه و دانش عماد چابک به همراه پشتکار و امید به آینده بود. اما حالا ما یک تیم 29 نفره هستیم که با انگیزه چند برابری نسبت به دیگران، تلاش می‌کنیم تا به کسب و کارها کمک کنیم که جزو بهترین ها نه، بلکه بهترین در حوزه کاری خود باشند و تاکنون با نتایجی که بدست آوردهایم، به خواسته خود جامه عمل پوشانده‌ایم.
+          </p>
+          <Link href={'/'} className="lg:font-bold font-light bg-purple text-whate lg:w-64 w-64 block lg:py-3 py-1 text-center rounded-lg ">معرفی کامل شرکت تجارت نوین <AiOutlineArrowLeft className="inline text-whate" size={17}/></Link>
         </div>
       </div>
     </>
