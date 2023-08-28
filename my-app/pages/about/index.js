953f56b41e78ad902ef0eefd74dd9a384e817f7a
amirhossein <developer.amir.khodamoradi@gmail.com>
2023-08-28 18:49:09 +0330

    V1.1.58

--- my-app/pages/about/index.js
+++ my-app/pages/about/index.js
@@ -7,8 +7,7 @@ import { AiOutlineHistory } from "react-icons/ai";
 import { FaShoppingCart } from "react-icons/fa";
 import { HiMiniUser } from "react-icons/hi2";
 import { TbSeeding } from "react-icons/tb";
-import { TbTrophyFilled , TbRosetteFilled } from "react-icons/tb";
-
+import { TbTrophyFilled, TbRosetteFilled } from "react-icons/tb";
 
 export default function about() {
   return (
@@ -26,23 +25,56 @@ export default function about() {
             مقدمات موفقیت شما را فراهم سازیم. مسیر رشد از همین‌جا شروع می‌شود.
             پس همین الان با یک تماس، قدم اول را محکم‌تر از همیشه بردارید.
           </p>
-        <div className="lg:flex grid grid-cols-2 md:grid-cols-3 gap-4 mb-4 mt-8">
-          <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <AiOutlineHistory className="inline"/> تاریخچه </p>
-         <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <TbTrophyFilled className="inline"/> نمونه کارهای ما</p>
-          <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <TbRosetteFilled className="inline"/> تجربه کاری</p>
-          <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <HiMiniUser className="inline"/> مشتریان</p>
-          <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <FaShoppingCart className="inline"/> محصولات ما</p>
-          <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular"> <TbSeeding className="inline"/> مسٔولیت اجتماعی</p>
-        </div>
+          <div className="lg:flex grid grid-cols-2 md:grid-cols-3 gap-4 mb-4 mt-8">
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <AiOutlineHistory className="inline" /> تاریخچه{" "}
+            </p>
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <TbTrophyFilled className="inline" /> نمونه کارهای ما
+            </p>
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <TbRosetteFilled className="inline" /> تجربه کاری
+            </p>
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <HiMiniUser className="inline" /> مشتریان
+            </p>
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <FaShoppingCart className="inline" /> محصولات ما
+            </p>
+            <p className="rounded-xl bg-purple w-full duration-200 hover:duration-200 hover:bg-primaryColor block text-center py-1 text-whate font-Regular">
+              {" "}
+              <TbSeeding className="inline" /> مسٔولیت اجتماعی
+            </p>
+          </div>
         </div>
       </section>
-      <section className="my-8 container mx-auto">
-        <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
-          <Image src={logo} />
-          <p>dsfsdfd</p>
-          <p>dsfsdfd</p>
-          <Image src={logo} />
+      <section className="my-8">
+        <div className="container mx-auto">
+          <div className="grid lg:grid-cols-2 grid-cols-1 gap-8">
+            <div>
+              <p className="font-Medium text-primaryColor text-4xl">
+                تاریخچه شرکت
+              </p>
+              <p className="font-light text-justify mt-6">
+                بدون شک استفاده از خدمات حرفه‌ای سئو به خوبی می‌تواند جایگاه ما
+                را در نتایج جستجوی کاربران بهبود دهد. از سوی دیگر با آموزش و
+                یادگیری سئو می‌توانیم اقدام به بهینه سازی صفحات سایت و در نتیجه
+                ارتقای رتبه صفحات سایت خود در نتایج جستجو کنیم. ما مصمم هستیم که
+                می‌توانیم مقدمات موفقیت شما را فراهم سازیم. مسیر رشد از همین‌جا
+                شروع می‌شود. پس همین الان با یک تماس، قدم اول را محکم‌تر از
+                همیشه بردارید. تاریخچه نمونه کارهای ما تجربه کاری
+              </p>
+              <Image src={logo} className="rounded-xl w-3/4 h-36 mt-14" />
+            </div>
+            <Image src={logo} className="rounded-3xl w-full h-96" />
+          </div>
         </div>
+        
       </section>
     </>
   );
